527cd562b5f3123b55e4f191e2ac15ad007364fc
Александр Плохой <alexplohoy@mail.ru>
2023-04-27 20:07:08 +0300

    add lombok boilerplate

--- src/main/java/com/example/awssample/entity/Person.java
+++ src/main/java/com/example/awssample/entity/Person.java
@@ -2,9 +2,15 @@ package com.example.awssample.entity;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBDocument;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @DynamoDBDocument
 public class Person implements Serializable {
 

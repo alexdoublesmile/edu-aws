eb17e55b7098e4633ba1344e8df5637bde8be996
Александр Плохой <alexplohoy@mail.ru>
2023-04-27 20:15:41 +0300

    specify dynamo config

--- src/main/java/com/example/awssample/config/DynamoDBConfig.java
+++ src/main/java/com/example/awssample/config/DynamoDBConfig.java
@@ -1,7 +1,24 @@
 package com.example.awssample.config;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DynamoDBConfig {
+
+    public DynamoDBMapper mapper() {
+        return new DynamoDBMapper(amazonDynamoDB());
+    }
+
+    private AmazonDynamoDB amazonDynamoDB() {
+        return AmazonDynamoDBClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("", ""))
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials("", "")))
+                .build();
+    }
 }

65360516a6a31820241d8ab7674f9c0a6217e442
Александр Плохой <alexplohoy@mail.ru>
2023-04-27 20:21:05 +0300

    specify add person to repository

--- src/main/java/com/example/awssample/repository/PersonRepository.java
+++ src/main/java/com/example/awssample/repository/PersonRepository.java
@@ -1,7 +1,17 @@
 package com.example.awssample.repository;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.example.awssample.entity.Person;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class PersonRepository {
+    private final DynamoDBMapper mapper;
+
+    public Person addPerson(Person person) {
+        mapper.save(person);
+        return person;
+    }
 }

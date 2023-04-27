a26fa45f370359b9b3f2e02c7f50a5028938e801
Александр Плохой <alexplohoy@mail.ru>
2023-04-27 20:05:40 +0300

    mark address dynamo components

--- src/main/java/com/example/awssample/entity/Address.java
+++ src/main/java/com/example/awssample/entity/Address.java
@@ -1,10 +1,17 @@
 package com.example.awssample.entity;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBDocument;
+
 import java.io.Serializable;
 
+@DynamoDBDocument
 public class Address implements Serializable {
-
+    
+    @DynamoDBAttribute
     private Long code;
+    @DynamoDBAttribute
     private String city;
+    @DynamoDBAttribute
     private String state;
 }

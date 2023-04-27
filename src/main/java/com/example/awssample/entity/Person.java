65483a83cfd04ab24c0ab585fbbc44bf63212224
Александр Плохой <alexplohoy@mail.ru>
2023-04-27 20:03:04 +0300

    add person entity

---
+++ src/main/java/com/example/awssample/entity/Person.java
@@ -0,0 +1,11 @@
+package com.example.awssample.entity;
+
+import java.io.Serializable;
+
+public class Person implements Serializable {
+
+    private String id;
+    private String name;
+    private String email;
+    private Integer age;
+}

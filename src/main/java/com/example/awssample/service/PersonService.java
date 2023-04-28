22ae469c1139b06bf96f6466ccc5f069c42237cc
Александр Плохой <alexplohoy@mail.ru>
2023-04-28 18:35:15 +0300

    add person service deps

--- src/main/java/com/example/awssample/service/PersonService.java
+++ src/main/java/com/example/awssample/service/PersonService.java
@@ -1,7 +1,12 @@
 package com.example.awssample.service;
 
+import com.example.awssample.repository.PersonRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class PersonService {
+
+    private final PersonRepository personRepository;
 }

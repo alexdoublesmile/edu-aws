e0003ef4eb9804103dc2af96ea75fc387072d7ea
Александр Плохой <alexplohoy@mail.ru>
2023-04-28 18:37:49 +0300

    specify add person to controller

--- src/main/java/com/example/awssample/controller/PersonController.java
+++ src/main/java/com/example/awssample/controller/PersonController.java
@@ -1,12 +1,21 @@
 package com.example.awssample.controller;
 
+import com.example.awssample.entity.Person;
 import com.example.awssample.service.PersonService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/persons")
 public class PersonController {
 
     private final PersonService personService;
+
+    @PostMapping
+    public Person save(Person person) {
+        return personService.add(person);
+    }
 }

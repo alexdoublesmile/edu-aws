0aa3df986ebcebd8978a226125702bfc7a942592
Александр Плохой <alexplohoy@mail.ru>
2023-04-28 18:35:43 +0300

    add person controller deps

--- src/main/java/com/example/awssample/controller/PersonController.java
+++ src/main/java/com/example/awssample/controller/PersonController.java
@@ -1,7 +1,12 @@
 package com.example.awssample.controller;
 
+import com.example.awssample.service.PersonService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class PersonController {
+
+    private final PersonService personService;
 }

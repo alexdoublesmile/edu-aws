606dfe53e6e155f5bc036f79dc885918f308d461
Александр Плохой <alexplohoy@mail.ru>
2023-05-03 18:07:27 +0300

    add service methods template

--- src/main/java/com/example/awssample/service/PersonService.java
+++ src/main/java/com/example/awssample/service/PersonService.java
@@ -1,5 +1,6 @@
 package com.example.awssample.service;
 
+import com.example.awssample.entity.Person;
 import com.example.awssample.repository.PersonRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,20 @@ import org.springframework.stereotype.Service;
 public class PersonService {
 
     private final PersonRepository personRepository;
+
+    public Person add(Person person) {
+        return null;
+    }
+
+    public Person findById(String id) {
+        return null;
+    }
+
+    public Person delete(String id) {
+        return null;
+    }
+
+    public Person update(Person person) {
+        return null;
+    }
 }
